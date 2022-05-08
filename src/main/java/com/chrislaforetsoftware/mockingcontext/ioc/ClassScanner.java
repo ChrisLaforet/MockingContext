aa1e12a482d7540a2b72acfd637a7ad10a0da841
Chris Laforet <laforet@chrislaforetsoftware.com>
2022-05-08 17:03:18 -0400

    Building up the class scanner

--- src/main/java/com/chrislaforetsoftware/mockingcontext/ioc/ClassScanner.java
+++ src/main/java/com/chrislaforetsoftware/mockingcontext/ioc/ClassScanner.java
@@ -1,16 +1,26 @@
 package com.chrislaforetsoftware.mockingcontext.ioc;
 
 import com.chrislaforetsoftware.mockingcontext.annotation.IAnnotationScanner;
+import com.chrislaforetsoftware.mockingcontext.annotation.impl.SpringAnnotationScanner;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ClassScanner {
 
-	public static ClassComponents decomposeClass(Class<?> theClass, IAnnotationScanner springAnnotationScanner) throws NoSuchMethodException {
+	private static final SpringAnnotationScanner springAnnotationScanner = new SpringAnnotationScanner();
+
+	public static Optional<ClassComponents> decomposeClass(Class<?> theClass, IAnnotationScanner springAnnotationScanner) throws NoSuchMethodException {
+
+		// determine if this class is eligible
+		if (!isClassAnnotatedAsComponent(theClass)) {
+			return Optional.empty();
+		}
+
 		final List<String> dependencies = new ArrayList<>();
 
 		/* TODO: determine alternate forms of the dependency based on implementation like
@@ -26,12 +36,32 @@ public class ClassScanner {
 
 		// do we have autowired fields in this class?
 		final Field [] fields = theClass.getFields();
+		final List<Field> autowiredDeclaredFields = loadAutowiredFields(fields);
+
 		final List<Field> autowiredDeclaredFields = new ArrayList<>();
 
 		final Constructor<?> defaultConstructor = theClass.getConstructor();
+		final Constructor<?>[] constructors = theClass.getConstructors();
+		boolean isOnlyDefault = false;
+		if (constructors.length == 1) {
+			if (constructors[0].getParameterTypes().length == 0) {
+				isOnlyDefault = true;
+			}
+		} else if (constructors.length == 0) {
+			isOnlyDefault = true;
+		}
+
+		if (isOnlyDefault) {
+			if (!autowiredDeclaredFields.isEmpty()) {
+				defaultConstructor.setAccessible(true);
+				final ClassComponents components = new ClassComponents(theClass, defaultConstructor, autowiredDeclaredFields);
+
+				return Optional.of(components);
+			}
+			return Optional.empty();
+		}
 
 		Constructor<?> autowiredConstructor = null;
-		final Constructor<?>[] constructors = theClass.getConstructors();
 		if (constructors.length == 1 && constructors[0] != defaultConstructor) {
 			for (Constructor<?> constructor : constructors) {
 				if (constructor == defaultConstructor) {
@@ -45,10 +75,19 @@ public class ClassScanner {
 		}
 
 
-		defaultConstructor.setAccessible(true);
 		return new ClassComponents(theClass, defaultConstructor, autowiredDeclaredFields);
 
 
 		return dependencies;
+	}
+
+	private static List<Field> loadAutowiredFields(Field [] fields) {
+		final List<Field> autowiredFields = new ArrayList<>();
+
+		return autowiredFields;
+	}
+
+	private static boolean isClassAnnotatedAsComponent(Class<?> theClass) {
+		return springAnnotationScanner.isAnnotatedAsTarget(theClass);
 	}
 }
